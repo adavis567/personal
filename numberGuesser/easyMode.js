f25471b3a940f55ee6547243b86eed578dc92aa5
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-08-11 20:15:03 +0000

    got sumbit button to work, need to get tries to work

--- numberGuesser/easyMode.js
+++ numberGuesser/easyMode.js
@@ -1,5 +1,15 @@
 function rand() {
-  var randNum = Math.round(Math.random() * 10)
-    document.getElementById("randNum").innerHTML = randNum;;
- 
+  var randNum = Math.ceil(Math.random() * 10)
+  document.getElementById("randNum").innerHTML = randNum;;
+
+}
+function guess() {
+  randNum = document.getElementById('randNum').innerHTML
+  var guess = document.getElementById('guess').value
+  if (guess === randNum) {
+    alert(true)
+  }
+  else {
+    alert(false)
+  }
 }
