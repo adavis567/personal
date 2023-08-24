72a60872a6ba2617b7df5cda2bff83ffa1e00de3
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-08-24 03:22:17 +0000

    homie at chatgpt made code better

--- numberGuesser/easyMode.js
+++ numberGuesser/easyMode.js
@@ -1,26 +1,51 @@
-var tries = 0
-document.getElementById('tries').innerHTML = tries
+let tries = 0;
+const maxTries = 3;
+
+document.getElementById('tries').innerHTML = tries;
+
 function rand() {
-  var randNum = Math.ceil(Math.random() * 10)
-  document.getElementById("randNum").innerHTML = randNum;;
-
+  const randNum = Math.floor(Math.random() * 10) + 1;
+  document.getElementById("randNum").innerHTML = randNum;
 }
+
 function guess() {
-  randNum = document.getElementById('randNum').innerHTML
-  var guess = document.getElementById('guess').value
+  const randNum = parseInt(document.getElementById('randNum').innerHTML, 10);
+  const guess = parseInt(document.getElementById('guess').value, 10);
+
   if (guess === randNum) {
-    alert(true)
+    showWinnerScreen();
+  } else {
+    tries++;
+    document.getElementById('tries').innerHTML = tries;
+    if (tries === maxTries) {
+      showLoserScreen(randNum);
+    } else {
+      showMessage('Try again.');
+    }
   }
-  else {
-    tries = tries + 1
-    document.getElementById('tries').innerHTML = tries
-    if (tries === 3) {
-  alert('loser')
-}
-else {
-  alert('try again')
-}
-  }
+
+  // Clear the input field after guess
+  document.getElementById('guess').value = '';
 }
 
+function showMessage(message) {
+  const messageDiv = document.getElementById('message');
+  messageDiv.innerHTML = message;
+}
 
+function showWinnerScreen() {
+  document.getElementById('winner-screen').classList.remove('hidden');
+}
+
+function showLoserScreen(correctNumber) {
+  document.getElementById('loser-screen').classList.remove('hidden');
+  document.getElementById('correct-number').textContent = correctNumber;
+}function resetGame() {
+  // Reset tries, hide screens, and clear messages and input
+  tries = 0;
+ location.reload();
+
+  
+  // Reset or regenerate random number if needed
+  rand();
+}
