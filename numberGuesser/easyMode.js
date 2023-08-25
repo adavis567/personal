2912dedb807fd657216a9c8d83f49e46c35725d6
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-08-25 20:20:24 +0000

    got the game to say whether number is higher or lower

--- numberGuesser/easyMode.js
+++ numberGuesser/easyMode.js
@@ -6,15 +6,27 @@ document.getElementById('tries').innerHTML = tries;
 function rand() {
   const randNum = Math.floor(Math.random() * 10) + 1;
   document.getElementById("randNum").innerHTML = randNum;
+  document.getElementById('rand').classList.add('hidden')
 }
 
 function guess() {
   const randNum = parseInt(document.getElementById('randNum').innerHTML, 10);
   const guess = parseInt(document.getElementById('guess').value, 10);
 
-  if (guess === randNum) {
+  if (guess < randNum) {
+    alert('the number is GREATER than ' + guess)
+    tries++
+    document.getElementById('tries').innerHTML = tries;
+  }
+  else if (guess > randNum) {
+    alert('the number is LESS than ' + guess)
+    tries++
+    document.getElementById('tries').innerHTML = tries;
+  }
+  else if (guess === randNum) {
     showWinnerScreen();
-  } else {
+  } 
+  else {
     tries++;
     document.getElementById('tries').innerHTML = tries;
     if (tries === maxTries) {
