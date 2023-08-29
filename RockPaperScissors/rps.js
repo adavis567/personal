eb5878cf6282d9b6e9c02ec9318a128310b160cf
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-08-29 20:25:57 +0000

    added scoreboard for ties

--- RockPaperScissors/rps.js
+++ RockPaperScissors/rps.js
@@ -1,85 +1,93 @@
 var score = 0
 var computerScore = 0
-document.getElementById('score').innerHTML=score
-document.getElementById('compScore').innerHTML=computerScore
+var ties = 0
+document.getElementById('score').innerHTML = score
+document.getElementById('compScore').innerHTML = computerScore
+document.getElementById('ties').innerHTML = ties
 function rock() {
-    var randNum = Math.ceil(Math.random() * 3)
-    var choice
-    if (randNum === 1) {
-        choice = 'rock'
-    }
-    else if (randNum === 2) {
-        choice = 'paper'
-    }
-    else if (randNum === 3) {
-        choice = 'scissors'
-    }
+  var randNum = Math.ceil(Math.random() * 3)
+  var choice
+  if (randNum === 1) {
+    choice = 'rock'
+  }
+  else if (randNum === 2) {
+    choice = 'paper'
+  }
+  else if (randNum === 3) {
+    choice = 'scissors'
+  }
   if (choice === 'rock') {
-      alert('tie')
-    }
+    alert('tie')
+    ties = ties + 1
+    document.getElementById('ties').innerHTML = ties
+  }
   else if (choice === 'paper') {
     alert('loser')
     computerScore = computerScore + 1
-    document.getElementById('compScore').innerHTML=computerScore
+    document.getElementById('compScore').innerHTML = computerScore
   }
   else if (choice === 'scissors') {
-   alert('winner')
-   score = score + 1
-  document.getElementById('score').innerHTML=score
-   
+    alert('winner')
+    score = score + 1
+    document.getElementById('score').innerHTML = score
+
   }
 }
 function paper() {
-    var randNum = Math.ceil(Math.random() * 3)
-    var choice
-    if (randNum === 1) {
-        choice = 'rock'
-    }
-    else if (randNum === 2) {
-        choice = 'paper'
-    }
-    else if (randNum === 3) {
-        choice = 'scissors'
-    }
+  var randNum = Math.ceil(Math.random() * 3)
+  var choice
+  if (randNum === 1) {
+    choice = 'rock'
+  }
+  else if (randNum === 2) {
+    choice = 'paper'
+  }
+  else if (randNum === 3) {
+    choice = 'scissors'
+  }
   if (choice === 'rock') {
-      alert('winner')
-       score = score + 1
-  document.getElementById('score').innerHTML=score
-   
-    }
+    alert('winner')
+    score = score + 1
+    document.getElementById('score').innerHTML = score
+
+  }
   else if (choice === 'paper') {
     alert('tie')
+    ties = ties + 1
+    document.getElementById('ties').innerHTML = ties
   }
   else if (choice === 'scissors') {
-   alert('loser')
-   computerScore = computerScore + 1
-    document.getElementById('compScore').innerHTML=computerScore
+    alert('loser')
+    computerScore = computerScore + 1
+    document.getElementById('compScore').innerHTML = computerScore
   }
 }
 function scissors() {
-    var randNum = Math.ceil(Math.random() * 3)
-    var choice
-    if (randNum === 1) {
-        choice = 'rock'
-    }
-    else if (randNum === 2) {
-        choice = 'paper'
-    }
-    else if (randNum === 3) {
-        choice = 'scissors'
-    }
+  var randNum = Math.ceil(Math.random() * 3)
+  var choice
+  if (randNum === 1) {
+    choice = 'rock'
+  }
+  else if (randNum === 2) {
+    choice = 'paper'
+  }
+  else if (randNum === 3) {
+    choice = 'scissors'
+  }
   if (choice === 'rock') {
-      alert('loser')
-      computerScore = computerScore + 1
-    document.getElementById('compScore').innerHTML=computerScore
-    }
+    alert('loser')
+    computerScore = computerScore + 1
+    document.getElementById('compScore').innerHTML = computerScore
+  }
   else if (choice === 'paper') {
     alert('winner')
-     score = score + 1
-  document.getElementById('score').innerHTML=score
-   
+    score = score + 1
+    document.getElementById('score').innerHTML = score
+
   }
   else if (choice === 'scissors') {
-   alert('tie')
+    alert('tie')
+    ties = ties + 1
+    document.getElementById('ties').innerHTML = ties
   }
 }
