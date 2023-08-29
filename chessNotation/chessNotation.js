4e277715f0ede3da0640c369300146f452c041c0
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-08-29 19:54:20 +0000

    added javascript and css

---
+++ chessNotation/chessNotation.js
@@ -0,0 +1,9 @@
+var piece
+function king() {
+    piece = 'k'
+    document.getElementById("notation").innerHTML=piece
+}
+function queen() {
+    piece='q'
+    document.getElementById("notation").innerHTML=piece
+}
