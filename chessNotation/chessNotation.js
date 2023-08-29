5bca48c9bcdd9625254a2190711f7cff90ec42f9
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-08-29 20:11:50 +0000

    working on input

--- chessNotation/chessNotation.js
+++ chessNotation/chessNotation.js
@@ -1,9 +1,12 @@
 var piece
+
 function king() {
     piece = 'k'
     document.getElementById("notation").innerHTML=piece
+    document.getElementById('notation').classList.remove('hidden');
 }
 function queen() {
     piece='q'
     document.getElementById("notation").innerHTML=piece
+    document.getElementById('notation').classList.remove('hidden');
 }
