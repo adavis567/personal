125364681020447cb321d3a40212505a81927866
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-09-07 19:27:16 +0000

    basic of page complete

---
+++ OfflineTv Descriptions/otvDesc.js
@@ -0,0 +1,18 @@
+function descriptions() {
+    document.getElementById("descriptions").classList.remove("hidden")
+    document.getElementById("welcome").classList.add("hidden")
+    document.getElementById("selection").classList.add("hidden")
+}
+
+function quiz() {
+    document.getElementById("quiz").classList.remove("hidden")
+    document.getElementById("welcome").classList.add("hidden")
+    document.getElementById("selection").classList.add("hidden")
+}
+
+function home() {
+    document.getElementById("descriptions").classList.add("hidden")
+    document.getElementById("quiz").classList.add("hidden")
+    document.getElementById("welcome").classList.remove("hidden")
+    document.getElementById("selection").classList.remove("hidden")
+}
