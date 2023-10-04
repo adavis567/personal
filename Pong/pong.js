9fe8143e5e7a5c6a7380c5bbb084e84538685df2
anthony davis <anthony_davis.stu@willowschoolnola.org>
2023-10-04 19:47:28 +0000

    trying to get ball colisions working

--- Pong/pong.js
+++ Pong/pong.js
@@ -14,10 +14,12 @@ function runProgram() {
         "W": 87,
         "S": 83,
         "UP": 38,
-        "DOWN": 40
+        "DOWN": 40,
+        "SPACE": 32
     }
 
     var player1 = {
+       
         "y-coordinate": 300,
         "y-speed": 0
     }
@@ -33,7 +35,7 @@ function runProgram() {
     }
     var walls = {
         "leftWall": 0,
-        "rightWall": $("#gameArea").width() - 50,
+        "rightWall": $("#gameArea").width() ,
         "topWall": 0,
         "bottomWall": $("#gameArea").height() - 180
     }
@@ -49,6 +51,7 @@ function runProgram() {
     function newFrame() {
         repositionGameItem()
         wallCollision()
+        paddleCollision()
         redrawGameItem()
     }
 
@@ -59,6 +62,16 @@ function runProgram() {
         else if (event.which === KEY.S) {
             player1["y-speed"] = 5
         }
+        if (event.which === KEY.UP) {
+            player2["y-speed"] = -5
+        }
+        else if (event.which === KEY.DOWN) {
+            player2["y-speed"] = 5
+        }
+        if (event.which === KEY.SPACE) {
+            ball["x-speed"] = 5
+            ball["y-speed"] = 5
+        }
     }
 
     function handleKeyUp(event) {
@@ -67,6 +80,12 @@ function runProgram() {
         }
         else if (event.which === KEY.S) {
             player1["y-speed"] = 0
+        }
+        if (event.which === KEY.UP) {
+            player2["y-speed"] = 0
+        }
+        else if (event.which === KEY.DOWN) {
+            player2["y-speed"] = 0
         }
     }
 
@@ -85,11 +104,17 @@ function runProgram() {
     }
     function repositionGameItem() {
         player1["y-coordinate"] += player1["y-speed"]
-        player1["y-coordinate"] += player1["y-speed"]
+        player2["y-coordinate"] += player2["y-speed"]
+        ball["x-coordinate"] += ball["x-speed"]
+        ball["y-coordinate"] += ball["y-speed"]
+
     }
 
     function redrawGameItem() {
         $("#player1").css("top", player1["y-coordinate"])
+        $("#player2").css("top", player2["y-coordinate"])
+        $("#ball").css("left", ball["x-coordinate"])
+        $("#ball").css("top", ball["y-coordinate"])
     }
 
     function wallCollision() {
@@ -99,7 +124,29 @@ function runProgram() {
         else if (walls.bottomWall <= player1["y-coordinate"]) {
             player1["y-coordinate"] = walls.bottomWall
         }
+        if (walls.topWall >= player2["y-coordinate"]) {
+            player2["y-coordinate"] = 0
+        }
+        else if (walls.bottomWall <= player2["y-coordinate"]) {
+            player2["y-coordinate"] = walls.bottomWall
+        }
+        
+
+       
 
     }
-    console.log(walls.bottomWall)
+
+    function paddleCollision() {
+        if (player1["y-coordinate"] === ball["x-coordinate"]) {
+            ball["x-coordinate"] -= ball["x-speed"]
+            ball["y-coordinate"] -= ball["y-speed"]
+        }
+        else if (walls.rightWall <= ball["x-coordinate"]) {
+            ball["x-coordinate"] -= ball["x-speed"]
+            ball["y-coordinate"] -= ball["y-speed"]
+        }
+        else if (walls.bottomWall <= ball["y-coordinate"]) {
+            ball["y-speed"] *= 1
+        }
+    }
 }
