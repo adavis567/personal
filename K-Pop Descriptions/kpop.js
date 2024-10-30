c9d00f42d01a18a6bcf63d2b5bce6b10615f9cda
adavis567 <135252642+adavis567@users.noreply.github.com>
2024-10-30 22:48:39 +0000

    got new jeans buttons working, not very ituitive tho

--- K-Pop Descriptions/kpop.js
+++ K-Pop Descriptions/kpop.js
@@ -1,9 +1,31 @@
-function newJeans() {
-    document.getElementById("groups").classList.add("hidden")
-    document.getElementById("newjeans").classList.remove("hidden")
-}
-
 function home() {
     document.getElementById("newjeans").classList.add("hidden")
     document.getElementById("groups").classList.remove("hidden")
+
+}
+
+function newJeans() {
+    document.getElementById("groups").classList.add("hidden")
+    document.getElementById("newjeans").classList.remove("hidden")
+    document.getElementById("njInfo").classList.add("hidden")
+    document.getElementById("njMembers").classList.add("hidden")
+    document.getElementById("njSongs").classList.add("hidden")
+}
+
+function njInfo() {
+    document.getElementById("njInfo").classList.remove("hidden")
+    document.getElementById("njMembers").classList.add("hidden")
+    document.getElementById("njSongs").classList.add("hidden")
+}
+
+function njMembers() {
+    document.getElementById("njMembers").classList.remove("hidden")
+    document.getElementById("njInfo").classList.add("hidden")
+    document.getElementById("njSongs").classList.add("hidden")
+}
+
+function njSongs() {
+    document.getElementById("njSongs").classList.remove("hidden")
+    document.getElementById("njInfo").classList.add("hidden")
+    document.getElementById("njMembers").classList.add("hidden")
 }
