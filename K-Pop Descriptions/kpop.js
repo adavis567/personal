7e66097c62a77a1ef02eef6c2863ee8825407f9c
adavis567 <135252642+adavis567@users.noreply.github.com>
2024-10-30 22:08:55 +0000

    got a buton working

--- K-Pop Descriptions/kpop.js
+++ K-Pop Descriptions/kpop.js
@@ -2,3 +2,8 @@ function newJeans() {
     document.getElementById("groups").classList.add("hidden")
     document.getElementById("newjeans").classList.remove("hidden")
 }
+
+function home() {
+    document.getElementById("newjeans").classList.add("hidden")
+    document.getElementById("groups").classList.remove("hidden")
+}
