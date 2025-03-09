f804b1cc726346007a782a92ff80c5623c2c60c5
anthony davis <man36600@gmail.com>
2025-03-09 23:02:07 +0000

    got info button working

--- K-Pop Descriptions/kpop.js
+++ K-Pop Descriptions/kpop.js
@@ -1,3 +1,24 @@
+function kpopInfo(debut, members, desc, fandom, awards, label) {
+    var kpop = {}
+    kpop.debut = debut
+    kpop.members = members
+    kpop.desc = desc
+    kpop.fandom = fandom
+    kpop.awards = awards
+    kpop.label = label
+    return kpop
+}
+
+var NewJeans = kpopInfo(
+    "2022",
+    "Minji, Hanni, Haerin, Danielle, Hyein",
+    "NewJeans is a South Korean girl group under ADOR, a subsidiary of HYBE. They debuted on July 22, 2022, with the track “Attention” quickly gaining massive popularity for their fresh Y2K-inspired aesthetic and smooth R&B-infused pop sound. Known for hits like “Hype Boy,” “Ditto,” and “OMG,” NewJeans has redefined K-pop trends with their effortless style and nostalgic yet innovative music.",
+    "Bunnies",
+    "2020 Mama Awards, 2023 Golden Disc Awards, 2024 Asia Artist Awards",
+    "Ador"
+)
+
+
 function home() {
     document.getElementById("newJeans").classList.add("hidden")
     document.getElementById("njBackBut").classList.add("hidden")
@@ -24,7 +45,7 @@ function newJeans() {
     document.getElementById("njMemBut").classList.remove("hidden")
     document.getElementById("njInfoBut").classList.remove("hidden")
     document.getElementById("njSongsBut").classList.remove("hidden")
-    document.body.style.backgroundImage="url('Photos/newJeans.webp')"
+    document.body.style.backgroundImage = "url('Photos/newJeans.webp')"
 }
 
 function njMembers() {
@@ -63,8 +84,19 @@ function minji() {
 
 function njInfo() {
     document.getElementById("njInfo").classList.remove("hidden")
+    document.getElementById("njInfoBut").classList.add("hidden")
     document.getElementById("njMembers").classList.add("hidden")
+    document.getElementById("njMemBut").classList.add("hidden")
     document.getElementById("njSongs").classList.add("hidden")
+    document.getElementById("njSongsBut").classList.add("hidden")
+    document.getElementById("njBackBut").classList.remove("hidden")
+    document.getElementById("njInfo").innerHTML = 
+    "Debut: " + NewJeans.debut + "<br>" + 
+    "Members: " + NewJeans.members + "<br>" + 
+    "Description: " + NewJeans.desc + "<br" + 
+    "Fandom Name: " + NewJeans.fandom + "<br>" +
+    "Awards: " + NewJeans.awards + "<br>" +
+    "Label: " + NewJeans.label + "<br>"
 }
 
 function njSongs() {
