18cab177ddd3cdec048c9ac10917eb4ae8e16c15
Anthony Davis <man36600@gmail.com>
2026-08-06 10:48:14 -0500

    did you do it chapter 1

---
+++ Hello2.java
@@ -0,0 +1,11 @@
+// Filename Hello2.java
+// Written by Anthony Davis
+// Written on 8/6/26
+
+public class Hello2 {
+    /* This class demonstrates the use of the println() method 
+       to print the message Hello, world! */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!"); // Displays the string
+    }
+}
